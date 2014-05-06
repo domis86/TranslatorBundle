8affcc80f6d246a66390de08fa9f060d651a324a
domis86 <domis86@gmail.com>
2014-05-06 17:58:20 +0200

    fix ControllerListener scope

--- Resources/public/js/webDebugDialog.js
+++ Resources/public/js/webDebugDialog.js
@@ -42,7 +42,7 @@ function Domis86WebDebugDialogClass(aBackendMode, aAssetsBasePath) {
                         jQuery(this).dialog("close");
                     }
                 },
-                width: 1000,
+                width: 1200,
                 height: 500
             });
         }
