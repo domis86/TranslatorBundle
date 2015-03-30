e38020e73d281e9582fdd1b58aa48f3ed2234ed6
domis86 <domis86@gmail.com>
2015-03-30 11:43:47 +0200

    override also @translator.default service

--- DependencyInjection/Compiler/TranslatorCompilerPass.php
+++ DependencyInjection/Compiler/TranslatorCompilerPass.php
@@ -1,7 +1,6 @@
 <?php
 namespace Domis86\TranslatorBundle\DependencyInjection\Compiler;
 
-use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -18,20 +17,13 @@ class TranslatorCompilerPass implements CompilerPassInterface
             return;
         }
 
-        // Set domis86_translator.translator.parent service.
-        // It will be injected to domis86_translator.translator service (see services.yml).
+        // Set domis86_translator.translator.parent service which will be injected to domis86_translator.translator
+        $originalTranslatorDefinition = $container->getDefinition('translator.default');
+        $originalTranslatorDefinition->setPublic(false);
+        $container->setDefinition('domis86_translator.translator.parent', $originalTranslatorDefinition);
 
-        if ($container->hasAlias('translator')) {
-            // original translator is an alias.
-            $originalTranslatorAlias = new Alias((string)$container->getAlias('translator'), false);
-            $container->setAlias('domis86_translator.translator.parent', $originalTranslatorAlias);
-        } else {
-            // original translator is a definition.
-            $originalTranslatorDefinition = $container->getDefinition('translator');
-            $originalTranslatorDefinition->setPublic(false);
-            $container->setDefinition('domis86_translator.translator.parent', $originalTranslatorDefinition);
-        }
-        $container->setAlias('translator', 'domis86_translator.translator');
+        $container->setAlias('translator',         'domis86_translator.translator');
+        $container->setAlias('translator.default', 'domis86_translator.translator'); // this override is needed because in some places @translator.default is injected instead of @translator - like in @validator service, see: vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/config/validator.xml
 
         $parentTranslatorReference = new Reference('domis86_translator.translator.parent');
         $container->getDefinition('domis86_translator.translator')->addMethodCall(
